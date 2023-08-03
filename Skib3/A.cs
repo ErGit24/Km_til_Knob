1644f0db26da7d9deb04318904e0701c2cf95ca9
Erol <erol_git@outlook.dk>
2023-08-03 12:20:04 +0200

    Add project files.

---
+++ Skib3/A.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skib3
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Indtast hastighed i km/t: ");
+            string input = Console.ReadLine();
+
+            if (double.TryParse(input, out double fartIKMT))
+            {
+                B bInstance = new B();
+                double fartIknobs = bInstance.ConvertKmPerHourToKnots(fartIKMT);
+                Console.WriteLine($"Hastighed i knob: {fartIknobs}");
+            }
+            else
+            {
+                Console.WriteLine("Ugyldigt input. Indtast et tal for hastigheden i km/t.");
+            }
+
+            Console.WriteLine("Tryk på en tast for at afslutte...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Skib3/B.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skib3
+{
+    class B
+    {
+        public B()
+        {
+            // Default konstruktør uden input
+        }
+
+        public double ConvertKmPerHourToKnots(double kmITimen)
+        {
+            double knobs = kmITimen * 0.53996;
+            return knobs;
+        }
+
+        public double ConvertKnotsToKmPerHour(double knobs)
+        {
+            double kmITimen = knobs / 0.53996;
+            return kmITimen;
+        }
+    }
+}
